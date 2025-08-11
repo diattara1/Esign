0d26843d741950c02389bec1311fb0105d225738
Diattus <Grandali@hotmail.fr>
2025-08-11 19:37:17 +0000

    url modifier

--- backend/esign/urls.py
+++ backend/esign/urls.py
@@ -4,13 +4,15 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 from signature import views
+from signature.views.auth import verify_token
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/signature/', include('signature.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/verify-token/', views.verify_token, name='verify_token'),
+    path('api/verify-token/', verify_token, name='verify_token'),
 ]
 
 if settings.DEBUG:

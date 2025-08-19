e0296acba7acedb4f5cce2fd0dad11b8d8495f93
Diattus <Grandali@hotmail.fr>
2025-08-19 13:46:51 +0000

    reint mdp

--- backend/esign/settings.py
+++ backend/esign/settings.py
@@ -4,7 +4,7 @@ import base64
 import os
 import environ
 from datetime import timedelta
-
+from django.core.exceptions import ImproperlyConfigured
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
